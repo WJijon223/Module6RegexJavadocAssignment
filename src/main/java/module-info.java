9386e8d0ad0b3df5c7399cba3962a8e1f337465f
WJijon223 <williamjijon223@gmail.com>
2025-04-14 12:37:15 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module edu.farmingdale.module6regexjavadocassignment {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens edu.farmingdale.module6regexjavadocassignment to javafx.fxml;
+    exports edu.farmingdale.module6regexjavadocassignment;
+}
