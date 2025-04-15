9d6a1566f22d4d10405c5a024457f26278b847ec
WJijon223 <williamjijon223@gmail.com>
2025-04-14 20:48:49 -0400

    edited the layout of the GUI

---
+++ src/main/java/edu/farmingdale/module6regexjavadocassignment/RegistrationController.java
@@ -0,0 +1,21 @@
+package edu.farmingdale.module6regexjavadocassignment;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class RegistrationController {
+
+    @FXML
+    private TextField birthDateInput;
+
+    @FXML
+    private TextField emailInput;
+
+    @FXML
+    private TextField nameInput;
+
+    @FXML
+    private TextField zipCodeInput;
+
+}
+
