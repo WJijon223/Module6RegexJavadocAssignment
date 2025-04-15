e39a64680758a14f5683ef5daccc35117cdff761
WJijon223 <williamjijon223@gmail.com>
2025-04-14 21:19:49 -0400

    added an add button

--- src/main/java/edu/farmingdale/module6regexjavadocassignment/RegistrationController.java
+++ src/main/java/edu/farmingdale/module6regexjavadocassignment/RegistrationController.java
@@ -1,9 +1,13 @@
 package edu.farmingdale.module6regexjavadocassignment;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class RegistrationController {
+
+    @FXML
+    private Button addButton;
 
     @FXML
     private TextField birthDateInput;
